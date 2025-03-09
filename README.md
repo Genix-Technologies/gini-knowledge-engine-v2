b20edeb1e8571973806f9c13b0cbef66bc7e74bf
rvmahesangenix <mahesan.vanniaperumal@genixtec.com>
2025-03-10 00:33:49 +0530

    removed frontend

---
+++ README.md
@@ -0,0 +1,4 @@
+ sudo docker build -t kebackend .
+
+
+ sudo docker run -d -p 80:9380 kebackend 
--- web/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  // Umi
-  extends: [require.resolve('umi/eslint'), 'plugin:react-hooks/recommended'],
-};
